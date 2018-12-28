ba724ea6dc43dcaf7a074fd112912f5b8c690e8d
TMowka <deftassassin@gmail.com>
2018-12-28 22:36:40 +0300

    End of section 9

---
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { AlertifyService } from '../../services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService, private route: ActivatedRoute, private alertifyService: AlertifyService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    });
+  }
+
+  // loadUsers() {
+  //   this.userService.getUsers().subscribe((users: User[]) => {
+  //     this.users = users;
+  //   }, error => {
+  //       this.alertifyService.error(error);
+  //   });
+  // }
+}
