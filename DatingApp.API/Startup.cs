c832ebf59dbfb12e6fd3702256d318aa2f0e4e3f
TMowka <deftassassin@gmail.com>
2019-01-06 15:40:02 +0300

    End of section 17

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -20,6 +20,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DatingApp.API
 {
@@ -34,6 +35,37 @@ namespace DatingApp.API
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(opt => opt
+        .UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+        .ConfigureWarnings(warn => warn.Ignore(CoreEventId.IncludeIgnoredWarning)));
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+        .AddJsonOptions(opt =>
+        {
+          opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        });
+      services.AddCors();
+      services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+      services.AddAutoMapper();
+      services.AddTransient<Seed>();
+      services.AddScoped<IAuthRepository, AuthRepository>();
+      services.AddScoped<IDatingRepository, DatingRepository>();
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(opt =>
+        {
+          opt.TokenValidationParameters = new TokenValidationParameters
+          {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+              .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+          };
+        });
+      services.AddScoped<LogUserActivity>();
+    }
+
+    public void ConfigureDevelopmentServices(IServiceCollection services)
     {
       services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
@@ -89,10 +121,18 @@ namespace DatingApp.API
       }
 
       // app.UseHttpsRedirection();
-      // seeder.SeedUsers();
+      seeder.SeedUsers();
       app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
       app.UseAuthentication();
-      app.UseMvc();
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+      app.UseMvc(routes =>
+      {
+        routes.MapSpaFallbackRoute(
+          name: "spa-fallback",
+          defaults: new { controller = "Fallback", action = "Index" }
+        );
+      });
     }
   }
 }
